6e2c4014e1d6705f528337898954cd5ec289514d
Yautrose <86356629+Yautrose@users.noreply.github.com>
2022-07-18 23:56:31 +0300

    Completed

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -8,32 +9,41 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        weatherManager.delegate = self
         searchTextField.delegate = self
-        
-    }
-
-    @IBAction func searchPressed (_ sender: UIButton) {
-        
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
         
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
         
+    }
+    
+}
+
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+    @IBAction func searchPressed (_ sender: UIButton) {
         searchTextField.endEditing(true)
-        
+        print(searchTextField.text!)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
         guard let text = searchTextField.text
         else {return false}
-        
         print(text)
         return true
     }
-    
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if textField.text != "" {
@@ -46,12 +56,48 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
         if let city = searchTextField.text {
             weatherManager.fetchWeather(cityName: city)
         }
-        
         searchTextField.text = ""
     }
+    
 }
 
+
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+            
+        }
+    }
+
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+
+}
+
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            //print(lat, lon)
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
--- Clima/Models/WeatherManager.swift
+++ Clima/Models/WeatherManager.swift
@@ -1,69 +1,61 @@
 import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
 
 struct WeatherManager {
     
     let url = "https://api.openweathermap.org/data/2.5/weather?appid=b324f10c7b750b245248cc6e35c1bfcb&units=metric"
+    var delegate: WeatherManagerDelegate?
     
     func fetchWeather(cityName: String) {
         let urlString = "\(url)&q=\(cityName)"
-        performReques(urlString: urlString)
+        performReques(with: urlString)
     }
     
-    func performReques(urlString: String) {
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(url)&lat=\(latitude)&lon=\(longitude)"
+        performReques(with: urlString)
+    }
+    
+    func performReques(with urlString: String) {
         // 1. Create a URL
         if let url = URL(string: urlString) {
             // 2. Create a URL session.
             let session = URLSession(configuration: .default)
-            
             // 3. Give the session a task
             let task = session.dataTask(with: url) { data, response, error in
-                
                 if error != nil {
-                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
-                
                 if let safeData = data {
-                    parseJSON(weatherData: safeData)
+                    if let weather = parseJSON(safeData) {
+                        delegate?.didUpdateWeather(self, weather: weather)
+                    }
                 }
             }
-            
             // 4. Start the task
             task.resume()
         }
     }
     
-    func parseJSON (weatherData: Data) {
+    func parseJSON (_ weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
             let id = decodeData.weather[0].id
-            print(getConditionName(weatherId: id))
+            let temp = decodeData.main.temp
+            let name = decodeData.name
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
         } catch {
-            print(error)
+            self.delegate?.didFailWithError(error: error)
+            return nil
         }
-    }
-    
-    func getConditionName(weatherId: Int) -> String {
-        switch weatherId {
-        case 200...232:
-            return "cloud.bolt"
-        case 300...321:
-            return "cloud.drizzle"
-        case 500...531:
-            return "cloud.rain"
-        case 600...622:
-            return "cloud.snow"
-        case 701...781:
-            return "cloud.fog"
-        case 800:
-            return "sun.max"
-        case 801...804:
-            return "cloud.bolt"
-        default:
-            return "cloud"
-        }
-        
     }
     
 }
